7cbbab91630c5de61f4c6f3ec788ea8f07ea00f9
Kacper <kacpersjuszb@gmail.com>
2023-11-23 22:32:53 +0100

    first commit

---
+++ src/main/java/com/brvsk/ZenithActive/ZenithActiveApplication.java
@@ -0,0 +1,13 @@
+package com.brvsk.ZenithActive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenithActiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenithActiveApplication.class, args);
+	}
+
+}

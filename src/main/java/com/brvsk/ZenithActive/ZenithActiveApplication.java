d7ffec77101cb130770dd33804dfb4934e652c98
Kacper <kacpersjuszb@gmail.com>
2024-01-19 13:18:13 +0100

    [new] save in db Instructor record

--- src/main/java/com/brvsk/ZenithActive/ZenithActiveApplication.java
+++ src/main/java/com/brvsk/ZenithActive/ZenithActiveApplication.java
@@ -2,13 +2,17 @@ package com.brvsk.ZenithActive;
 
 import com.brvsk.ZenithActive.security.auth.AuthenticationService;
 import com.brvsk.ZenithActive.security.auth.RegisterEmployeeRequest;
+import com.brvsk.ZenithActive.security.auth.RegisterInstructorRequest;
 import com.brvsk.ZenithActive.security.auth.RegisterMemberRequest;
 import com.brvsk.ZenithActive.user.Gender;
 import com.brvsk.ZenithActive.user.employee.EmployeeType;
+import com.brvsk.ZenithActive.user.instructor.Speciality;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ZenithActiveApplication {
@@ -47,6 +51,22 @@ public class ZenithActiveApplication {
 					.build();
 			System.out.println("Employee token: " +
 					authenticationService.registerEmployee(employee).getAccessToken());
+
+			RegisterInstructorRequest instructor = RegisterInstructorRequest
+					.builder()
+					.firstName("instructor")
+					.lastName("instructor")
+					.email("instructor@example.com")
+					.password("instructor")
+					.gender(Gender.OTHER)
+					.employeeType(EmployeeType.INSTRUCTOR)
+					.additionalInformation("instructor")
+					.description("instructor")
+					.specialities(List.of(Speciality.YOGA, Speciality.PILATES))
+					.build();
+
+			System.out.println("Instructor token: " +
+					authenticationService.registerInstructor(instructor).getAccessToken());
 
 		};
 	}
